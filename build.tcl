b3256b9200a30cb96a1c14a0d5bec88949782fcd
Danilo Chang <ray2501@gmail.com>
2018-01-22 16:39:27 +0800

    Update build.tcl

--- build.tcl
+++ build.tcl
@@ -3,11 +3,6 @@
 set arch "x86_64"
 set base "tktable-tktable"
 
-set fileurl "http://tktable.cvs.sourceforge.net/viewvc/tktable/tktable/?view=tar"
-
-set var [list wget $fileurl -O $base.tar.gz]
-exec >@stdout 2>@stderr {*}$var
-
 if {[file exists build]} {
     file delete -force build
 }
@@ -17,7 +12,3 @@ file copy -force $base.tar.gz build/SOURCES
 
 set buildit [list rpmbuild --target $arch --define "_topdir [pwd]/build" -bb tktable.spec]
 exec >@stdout 2>@stderr {*}$buildit
-
-# Remove source code package
-file delete $base.tar.gz
-
